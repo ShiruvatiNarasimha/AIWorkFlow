4fbc222cd1dcf8c9e26159b1d6528213e0f8dc53
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-01-23 21:10:01 +0530

    Updated SideBar

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -4,6 +4,11 @@ import { usePaths } from "@/hooks/user-nav";
 import { LogoSmall } from "@/svgs/logo-small";
 import React from "react";
 import Items from "./items";
+import { Separator } from "@/components/ui/separator";
+import ClerkAuthState from "../clerk-auth-state";
+import { HelpDuoToneWhite } from "@/icons";
+import { SubscriptionPlan } from "../subscription-plan";
+import UpgradeCard from "./upgrade";
 
 type Props = {
   slug: string;
@@ -46,9 +51,27 @@ const Sidebar = ({ slug }: Props) => {
         <div className="flex gap-x-2 items-center p-5 justify-center">
           <LogoSmall />
         </div>
-        <div className="flex flex-col py-3">
+        <div className="flex flex-col py-3 text-white">
           <Items page={page} slug={slug} />
         </div>
+        <div className="px-16">
+          <Separator orientation="horizontal" className="bg-[#333336]" />
+        </div>
+        <div className="px-3 flex flex-col gap-y-5">
+          <div className="flex gap-x-2">
+            <ClerkAuthState />
+            <p className="text-[#9B9CA0]">Profile</p>
+          </div>
+          <div className="flex gap-x-3">
+            <HelpDuoToneWhite />
+            <p className="text-[#9B9CA0]">Help</p>
+          </div>
+        </div>
+        <SubscriptionPlan type="FREE">
+          <div className="flex-1  flex flex-col justify-end">
+            <UpgradeCard />
+          </div>
+        </SubscriptionPlan>
       </div>
     </div>
   );
