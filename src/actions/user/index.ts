fd78c098e6e101c2c42cf17c55ec28dea11e309f
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-01-28 21:40:27 +0530

    Updated Schame

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -2,6 +2,8 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { findUser } from "./queries";
+import { refreshToken } from "@/lib/fetch";
 
 export const onCurrentUser = async () => {
   const user = await currentUser();
@@ -10,4 +12,23 @@ export const onCurrentUser = async () => {
   return user;
 };
 
-export const onBoardUser = async () => {};
+export const onBoardUser = async () => {
+  const user = await onCurrentUser();
+  try {
+    const found = await findUser(user.id);
+    if (found) {
+      if (found.integrations.length > 0) {
+        const today = new Date();
+        const time_left =
+          found.integrations[0].expiresAt?.getTime()! - today.getTime();
+
+        const days = Math.round(time_left / (1000 * 3600 * 24));
+        if (days < 5) {
+          console.log("refresh");
+
+          const refresh = await refreshToken(found.integrations[0]);
+        }
+      }
+    }
+  } catch (erroe) {}
+};
